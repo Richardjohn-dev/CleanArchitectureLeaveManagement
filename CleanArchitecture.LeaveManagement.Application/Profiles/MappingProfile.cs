b00489c04b7801b65c6d0dc880c9ab371cfb0240
Richard John <84065964+Richardjohn-dev@users.noreply.github.com>
2021-12-05 18:49:58 +0700

    Starting Infrastructure

--- CleanArchitecture.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ CleanArchitecture.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -12,8 +12,17 @@ namespace CleanArchitecture.LeaveManagement.Application.Profiles
         {
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+
+
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
+
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
         }
     }
 }
